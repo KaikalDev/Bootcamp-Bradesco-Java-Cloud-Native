3721ced63d1bf504bc05167be8c181ec2b26250d
Kaique <kaiquesimoes58@gmail.com>
2025-04-30 01:25:47 -0300

    Init

---
+++ RestAPI_nuvem/RestAPI_Nuvem/src/main/java/kaique/luan/dev/RestApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package kaique.luan.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiNuvemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiNuvemApplication.class, args);
+    }
+
+}
